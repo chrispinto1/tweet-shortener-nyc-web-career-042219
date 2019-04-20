27b6a1f4ba5d4efc18f0ef54a7fec7930aba9ccb
Christian Pinto <pintochristian2@gmail.com>
2019-04-20 20:43:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,13 +2,13 @@
 
 def dictionary
   word = {
-    "hello": "hi",
-    ["to","two","too"]: "2",
-    ["for","four"]: "4",
-    "be": "b",
-    "you": "u",
-    "at": "@",
-    "and": "&"
+    "hello" => "hi",
+    ["to","two","too"] => "2",
+    ["for","four"] => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
   word
 end
